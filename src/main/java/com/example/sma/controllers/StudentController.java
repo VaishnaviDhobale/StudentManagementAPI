da72801da9f7961267152575ecf961f23eb29d71
VaishnaviDhobale <masaieducation17@gmail.comVaishnaviDhobale@users.noreply.github.com>
2025-05-27 16:51:18 +0530

    add base path.

--- src/main/java/com/example/sma/controllers/StudentController.java
+++ src/main/java/com/example/sma/controllers/StudentController.java
@@ -5,17 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sma.models.StudentModel;
 import com.example.sma.services.StudentService;
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
     @Autowired
     private StudentService service;
